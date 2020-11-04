6eb4bf54d51d7e18d1f559ec7376c18d44efd870
Crowdbotics <team@crowdbotics.com>
2020-11-04 00:52:42 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList167396', human_name: 'Article List', access_route: 'ArticleList167396'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
