3409c0cfdbd78b9ad37be83b0da8d2d7845dad9a
Crowdbotics <team@crowdbotics.com>
2020-11-04 00:50:37 +0000

    Initial commit.

---
+++ backend/letters_for_lauren_22277/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for letters_for_lauren_22277 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letters_for_lauren_22277.settings')
+
+application = get_wsgi_application()
